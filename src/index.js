ad7ba0ee1a19aa27db52d73bcd63070e2c18ad29
ZhangYi <1173186987@qq.com>
2021-07-21 16:52:52 +0800

    城市选择中渲染左侧城市

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 //导入antd-mobile的样式
 import 'antd-mobile/dist/antd-mobile.css'
 
+import 'react-virtualized/styles.css'
+
 //导入字体图标库的样式文件
 import './assets/fonts/iconfont.css'
 
--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -4,6 +4,8 @@ import { NavBar } from 'antd-mobile'
 
 import axios from 'axios'
 
+import { List, AutoSizer } from 'react-virtualized'
+
 import './index.css'
 
 import { getCurrentCity } from '../utils/index'
@@ -31,7 +33,29 @@ const formatCityData = (list) => {
     }
 }
 
+// const list = Array(100).fill('react-virtualized')
+
+// 索引的高度
+const TITLE_HEIGHT = 36
+// 每个城市名称的高度
+const NAME_HEIGHT = 50
+
+const formatCityIndex = (letter) => {
+    switch (letter) {
+        case '#':
+            return '当前定位'
+        case 'hot':
+            return '热门城市'
+        default:
+            return letter.toUpperCase()
+    }
+}
+
 export default class CityList extends Component {
+    state = {
+        cityList: {},
+        cityIndex: []
+    }
     componentDidMount() {
         this.getCityList()
     }
@@ -50,8 +74,51 @@ export default class CityList extends Component {
 
         // 获取当前定位城市
         const curCity = await getCurrentCity()
+
+        // 1.将当前定位城市添加到 cityList 中
+        // 2.将当前定位城市的索引添加到 cityIndex 中
+        cityList['#'] = [curCity]
+        cityIndex.unshift('#')
         
-        console.log(cityList, cityIndex, curCity)
+        // console.log(cityList, cityIndex, curCity)
+        this.setState({
+            cityList,
+            cityIndex
+        })
+    }
+  
+    rowRenderer = ({
+        key,
+        index, // 索引项
+        isScrolling,
+        isVisible,
+        style
+    }) => {
+        // 获取每一行的字母索引
+        const { cityIndex, cityList } = this.state
+        const letter = cityIndex[index]
+        // 获取指定字符索引下的城市列表数据
+        // cityList[letter]
+
+        return (
+            <div
+                key={key}
+                style={style}
+                className="city"
+            >
+                <div className="title">{formatCityIndex(letter)}</div>
+                {
+                    cityList[letter].map(item => <div className="name" key={item.value}>
+                        {item.label}
+                    </div>)
+                }
+            </div>
+        )
+    }
+
+    getRowHeight = ({ index }) => {
+        const { cityList, cityIndex } = this.state
+        return TITLE_HEIGHT + cityList[cityIndex[index]].length * NAME_HEIGHT
     }
 
     render() {
@@ -64,6 +131,21 @@ export default class CityList extends Component {
                     icon={<i className="iconfont icon-back" />}
                     onLeftClick={() => this.props.history.go(-1)}
                 >城市选择</NavBar>
+
+                {/* 城市列表 */}
+                <AutoSizer>
+                    {
+                        ({ width, height }) => (
+                            <List
+                                width={width}
+                                height={height}
+                                rowCount={this.state.cityIndex.length}
+                                rowHeight={this.getRowHeight}
+                                rowRenderer={this.rowRenderer}
+                            />
+                        )
+                    }
+                </AutoSizer>
             </div>
         )
     }
